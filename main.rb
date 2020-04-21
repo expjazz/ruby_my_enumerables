f377f8de7af883324e8b0c7b87c4ef97dc43215e
Expedito Andrade <expeditojazz@gmail.com>
2020-04-21 17:36:41 -0300

    made changes required from TSE

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
 module Enumerable
-  # rubocop:disable Metrics/PerceivedComplexity
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
   def my_each
     i = 0
     if block_given?
-      while i <= length - 1
+      while i < length
         yield(self[i])
         i += 1
       end
@@ -27,65 +27,114 @@ module Enumerable
 
   def my_select
     arr = []
-    return to_enum if block_given? == false
+    return to_enum unless block_given?
 
     my_each { |x| arr << x if yield(x) == true }
     arr
   end
 
-  def my_all?
-    return to_enum unless block_given?
-
-    my_each { |x| return false if yield(x) == false }
+  def my_all?(arg = nil, &prc)
+    if block_given?
+      my_each { |x| return false if prc.call(x) == false }
+    elsif arg.is_a?(Regexp)
+      my_each { |x| return false if arg.match?(x.to_s) == false }
+    elsif arg.is_a?(Class)
+      my_each { |x| return false if x.is_a?(arg) == false }
+    elsif arg.nil? == false
+      my_each { |x| return false if x != arg }
+    else
+      my_each { |x| return false if x.nil? == true }
+    end
     true
   end
 
   def my_any?(&prc)
-    return to_enum unless block_given?
-
-    my_each { |x| return true if prc.call(x) }
+    if block_given?
+      my_each { |x| return true if prc.call(x) }
+    else
+      my_each { |x| return true if x.nil? == false }
+    end
     false
   end
 
-  def my_none?
-    return to_enum if block_given? == false
-
-    my_each { |x| return false if yield(x) == true }
+  def my_none?(arg = nil, &prc)
+    if block_given?
+      my_each { |x| return false if prc.call(x) == true }
+    elsif arg.is_a?(Regexp)
+      my_each { |x| return false if arg.match?(x.to_s) == true }
+    elsif arg.is_a?(Class)
+      my_each { |x| return false if x.is_a?(arg) == true }
+    elsif arg.nil? == false
+      my_each { |x| return false if x == arg }
+    else
+      my_each { |x| return false if x.nil? == false }
+    end
     true
   end
 
-  def my_count(&prc)
-    return to_enum unless block_given?
-
+  def my_count(arg = nil, &prc)
     count = 0
-    my_each { |x| count += 1 if prc.call(x) == true }
+    if block_given?
+      my_each { |x| count += 1 if prc.call(x) == true }
+    elsif block_given? == false && arg.nil?
+      my_each { |x| count += 1 if x.nil? == false }
+    elsif arg.nil? == false
+      my_each { |x| count += 1 if x == arg }
+    end
     count
   end
 
-  def my_map(prc = nil)
+  def my_map(&prc)
+    return to_enum unless block_given?
+
     arr = []
-    if block_given? && prc.nil?
-      my_each { |x| arr << yield(x) }
-    else
-      my_each { |x| arr << prc.call(x) }
-    end
+    my_each { |x| arr << prc.call(x) }
     arr
   end
 
-  def my_inject(num_1 = nil, &prc)
-    return to_enum unless block_given?
+  def my_inject(arg = nil, arg_2 = nil)
 
-    if num_1.nil?
-      my_each_with_index do |_ele, ind|
-        self[0] = prc.call(self[0], self[ind + 1]) if ind < length - 1
+    ind = 0
+    if block_given? == false
+      if arg.is_a?(Symbol)
+        while ind < length - 1
+          self[0] = self[0] + self[ind + 1] if arg[0] == '+'
+          self[0] = self[0] - self[ind + 1] if arg[0] == '-'
+          self[0] = self[0] * self[ind + 1] if arg[0] == '*'
+          self[0] = self[0] / self[ind + 1] if arg[0] == '/'
+          ind += 1
+        end
+      elsif arg.is_a?(Integer)
+        while ind < length - 1
+          self[0] = self[0] + arg if arg_2[0] == '+' && ind.zero?
+          self[0] = self[0] + self[ind + 1] if arg_2[0] == '+'
+          self[0] = self[0] - arg if arg_2[0] == '-' && ind.zero?
+          self[0] = self[0] - self[ind + 1] if arg_2[0] == '-'
+          self[0] = self[0] * arg if arg_2[0] == '*' && ind.zero?
+          self[0] = self[0] * self[ind + 1] if arg_2[0] == '*'
+          self[0] = self[0] / arg if arg_2[0] == '/' && ind.zero?
+          self[0] = self[0] / self[ind + 1] if arg_2[0] == '/'
+          ind += 1
+        end
       end
-    else
-      my_each_with_index do |_ele, ind|
-        self[0] = prc.call(num_1, self[0]) if ind.zero?
-        self[0] = prc.call(self[0], self[ind]) if ind.positive?
+    elsif block_given?
+      if arg.nil?
+        my_each_with_index do |_ele, ind|
+          self[0] = yield(self[0], self[ind + 1]) if ind < length - 1
+        end
+      else
+        my_each_with_index do |_ele, ind|
+          self[0] = yield(num_1, self[0]) if ind.zero?
+          self[0] = yield(self[0], self[ind]) if ind.positive?
+        end
       end
     end
     self[0]
   end
 end
-# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+def multiply_els(array)
+  array.my_inject{ |product, n| product - n }
+end
+p multiply_els([3, 2, 1])

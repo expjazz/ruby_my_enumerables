4f0c010f1cfda352f7ce4e301e577ea3de3ce8ab
Expedito Andrade <expeditojazz@gmail.com>
2020-04-21 10:15:05 -0300

    finished all methods

--- main.rb
+++ main.rb
@@ -1,41 +1,59 @@
 module Enumerable
     def my_each
-        for i in 0...self.size
-            yield(self[i])
-        end
-    self
+        if block_given?
+            for i in 0...self.size
+                yield(self[i])
+            end
+        return self
+        end         
+        to_enum
     end
 
     def my_each_with_index
-        for i in 0...self.size
-            yield(self[i], i)
+        if block_given?
+            for i in 0...self.size
+                yield(self[i], i)
+            end
+            return self
         end
-        self
+        to_enum
     end
     def my_select
+        return to_enum if block_given? == false
         arr = []
         self.my_each{|x| arr << x if yield(x) == true}
         arr
     end
     def my_all?
+        return to_enum unless block_given?
         self.my_each{|x| return false if yield(x) == false}
         return true
     end
     def my_none?
+        return to_enum if block_given? == false
         self.my_each{|x| return false if yield(x) == true}
         return true
     end
     def my_count(&prc)
+        return to_enum unless block_given?
         count = 0
         self.my_each{|x| count += 1 if prc.call(x) == true}
         count
     end
-    def my_map(&prc)
-        arr = Array.new
-        self.my_each{|x| arr << prc.call(x)}
-        arr
+    def my_map(prc=nil)
+        if block_given? && prc == nil
+            arr = Array.new
+            self.my_each{|x| arr << yield(x)}
+            arr
+        else
+
+            arr = Array.new
+            self.my_each{|x| arr << prc.call(x)}
+            arr
+        end
     end
     def my_inject(num_1=nil, &prc)
+        return to_enum unless block_given?
         if num_1 == nil
             self.my_each_with_index do |ele, ind|
                 if ind < self.size - 1
@@ -61,9 +79,5 @@ def multiply_els(array)
     array.my_inject {|acc, x| acc * x}
 end
 
-
-a = [2, 5, 7, 8].my_inject {|acc, x| acc + x}
-
-p a
-
-p multiply_els([2,4,5]) 
+test = Proc.new{|x| x + 5}
+p [2, 3, 4, 5].my_map {|x| x * 5}
